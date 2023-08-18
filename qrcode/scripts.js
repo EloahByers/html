5b9b0218ff8af2ba6f95c17d9663b1e83f115895
Eloah Byers <124396853+EloahByers@users.noreply.github.com>
2023-08-18 16:56:32 -0300

    Add files via upload

---
+++ qrcode/scripts.js
@@ -0,0 +1,39 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = container.querySelector("#qr-form input");
+const qrCodeImg = container.querySelector("#qr-code img");
+
+// Gerar código
+function generateQrCode() {
+  let qrCodeInputValue = qrCodeInput.value;
+
+  if (!qrCodeInputValue) return;
+
+  qrCodeBtn.innerText = "Gerando código...";
+
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+  qrCodeImg.addEventListener("load", () => {
+    container.classList.add("active");
+    qrCodeBtn.innerText = "Código criado!";
+  });
+}
+
+qrCodeBtn.addEventListener("click", () => {
+  generateQrCode();
+});
+
+qrCodeInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    generateQrCode();
+  }
+});
+
+// Limpar área do código
+qrCodeInput.addEventListener("keyup", () => {
+  if (!qrCodeInput.value) {
+    container.classList.remove("active");
+    qrCodeBtn.innerText = "Gerar QR Code";
+  }
+});
